82053015d6715d8c7bd1b1cb8d3424638ec0be44
Exla <jakubowskialexander@gmail.com>
2022-07-15 23:09:22 +0200

    Different Dice Types

--- GameScene/Objects/Dice.gd
+++ GameScene/Objects/Dice.gd
@@ -1,22 +1,32 @@
-extends Area2D
+extends Node2D
 
 
 # Declare member variables here. Examples:
 var eyes 
+var type
 
+var typeMap = ['Food','Fun','Education','Industry']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	eyes = randi() % 6 + 1
-	var imgPathToLoad = 'res://Assets/Graphics/DiceGraphics/dice'+str(eyes)+'.png'
+	type = randi() % 4
+	var imgPathToLoad = 'res://Assets/Graphics/DiceGraphics/'+typeMap[type]+'/dice'+str(eyes)+'.png'
 	$DiceSprite.texture = load(imgPathToLoad)
 
 
 
-func _input_event(viewport, event, shape_idx):
-	print('test')
-	if event.type == InputEvent.MOUSE_BUTTON \
-	and event.button_index == BUTTON_LEFT \
-	and event.pressed:
-		print("Clicked")
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		if $DiceSprite.get_rect().has_point(get_local_mouse_position()):
+			if event.pressed:
+				if GameManager.diceRerollsLeft > 0:
+					eyes = randi() % 6 + 1
+					var imgPathToLoad = 'res://Assets/Graphics/DiceGraphics/'+typeMap[type]+'/dice'+str(eyes)+'.png'
+					$DiceSprite.texture = load(imgPathToLoad)
+					GameManager.diceRerollsLeft -= 1
+				else:
+					print('no more rerolls')
+
+
