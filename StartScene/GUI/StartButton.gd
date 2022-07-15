d4cdb158f05d0dfade689ca4b0eec71e37428525
Jonathan Schmitz <jomitz96@hotmail.com>
2022-07-15 14:28:24 +0200

    GMTK 2022 #3: Add Scene Transition Manager

--- StartScene/GUI/StartButton.gd
+++ StartScene/GUI/StartButton.gd
@@ -1,5 +1,5 @@
 extends "res://GeneralScene/GUI/ScalingButton.gd"
 
 func on_pressed():
-	get_tree().change_scene("res://GameScene/GameScene.tscn")
+	TransitionManager.transitionTo("res://GameScene/GameScene.tscn")
 	SoundManager.playMusic()
