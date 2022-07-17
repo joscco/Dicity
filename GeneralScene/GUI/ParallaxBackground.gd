5e556415753bc7d24e173f90c1adeed0fd610461
Jonathan Schmitz <jomitz96@hotmail.com>
2022-07-17 10:19:43 +0200

    Delete more stuff

--- GeneralScene/GUI/ParallaxBackground.gd
+++ GeneralScene/GUI/ParallaxBackground.gd
@@ -2,4 +2,4 @@ extends ParallaxBackground
 	
 func _process(_delta):
 	var newOffset = get_scroll_base_offset() + Vector2(1, -0.5)
-	set_scroll_base_offset(newOffset) 
+	set_scroll_base_offset(newOffset)
